178b1b293d25c222ef3020b1effc92a27f521ea7
DayVil <dayvilgp@gmail.com>
2021-09-29 23:26:44 +0200

    Initial workings!

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from tkinter import filedialog
-from tkinter import *
+import fitz
 
+
+PATH = "./samples/"
+looked_word = "This"
 
 def main():
-    root = Tk()
-    root.title("Keyword Finder")
-    root.iconbitmap("")
+    pdf_name = "Recipes.pdf"
+    lk_word = looked_word.lower()
 
+    with fitz.open(PATH+pdf_name) as pages:
+        with open("log.md", "a") as f:
+                    f.write(f"Word found at pdf {pdf_name}\n\n")
+
+        for page in pages:
+            lis_txt = page.getText().lower().split(" ")
+            if lk_word in lis_txt:
+                with open("log.md", "a") as f:
+                    f.write(f"Word found at page: {page.number}\n")
+        
+        
 if __name__ == '__main__':
+    with open("log.md", "w") as f:
+        f.write("")
     main()
