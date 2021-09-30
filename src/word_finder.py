323492c03c6cf1c176fc9fb51768ce5d33d52b12
DayVil <dayvilgp@gmail.com>
2021-09-30 21:25:16 +0200

    UI work

--- src/word_finder.py
+++ src/word_finder.py
@@ -1,41 +1,43 @@
 import fitz
 import os
 
-PATH = "./res/samples/"
-looked_word = "This"
 
-
-def write_log(str):
+def _write_log(str):
     with open("log.md", "a") as f:
         f.write(str)
 
 
-def reset_log():
+def _reset_log():
     with open("log.md", "w") as f:
         f.write("")
 
 
-def find_word(path, pdf_name, lk_word):
+def _find_word(path, pdf_name, lk_word):
     with fitz.open(path+pdf_name) as pages:
         w = False
         for page in pages:
             lis_txt = page.getText().lower().strip().split()
             if lk_word in lis_txt:
                 if w is False:
-                    write_log(f"\nWord found at pdf {pdf_name}\n")
+                    _write_log(f"\nWord found at pdf {pdf_name}\n")
                     w = True
-                write_log(f"Word found at page: {page.number}\n")
+                _write_log(f"Word found at page: {page.number}\n")
 
 
-def main(path, lk_word):
+def finder(path, lk_word):
+    _reset_log()
+    
+    path += "/"
     lk_word = lk_word.lower()
     lis_dir = os.listdir(path=path)
 
     for file in lis_dir:
         if ".pdf" in file[-4:]:
-            find_word(path, file, lk_word)
+            _find_word(path, file, lk_word)
         
         
 if __name__ == '__main__':
-    reset_log()
-    main(PATH, looked_word)
+    PATH = "./res/samples/"
+    looked_word = "This"
+    
+    finder(PATH, looked_word)
